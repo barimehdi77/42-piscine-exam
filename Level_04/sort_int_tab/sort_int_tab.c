69a95647871d595a10d8b2adefd80ef4fd9496cb
João Rocha <71012028+CptCaveira@users.noreply.github.com>
2022-06-16 08:25:33 +0200

    Update sort_int_tab.c
    As it was, the code was failing moulinette,
    I can't be sure that checking for the NULL array passes it, but it doesn't hurt.

--- Level_04/sort_int_tab/sort_int_tab.c
+++ Level_04/sort_int_tab/sort_int_tab.c
@@ -4,15 +4,18 @@ void	sort_int_tab(int *tab, unsigned int size)
 	int tmp;
 
 	i = 0;
-	while (i < size)
+	if (tab != NULL)
 	{
-		if (tab[i] > tab[i + 1])
+		while (i < size)
 		{
-			tmp = tab[i];
-			tab[i] = tab[i + 1];
-			tab[i + 1] = tmp;
-			i = -1;
+			if (tab[i] > tab[i + 1])
+			{
+				tmp = tab[i];
+				tab[i] = tab[i + 1];
+				tab[i + 1] = tmp;
+				i = -1;
+			}
+			i++;
 		}
-		i++;
 	}
 }
